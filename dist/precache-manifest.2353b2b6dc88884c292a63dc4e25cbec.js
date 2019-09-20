c935519d88258bc299e3d03a7fb91b24a462f429
number2 <1029449742@qq.com>
2019-09-20 09:46:42 +0800

    提交dis 文件夹

---
+++ dist/precache-manifest.2353b2b6dc88884c292a63dc4e25cbec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "003a06277202c965d69265817ffa94ad",
+    "url": "/fonts/iconfont.003a0627.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "41795facd9d80df2b9da",
+    "url": "/js/chunk-vendors.b1773433.js"
+  },
+  {
+    "revision": "bd29c8e90a04d0567e34",
+    "url": "/js/app.d884108e.js"
+  },
+  {
+    "revision": "8ea41c61753dea2aeba3",
+    "url": "/js/about.551b747c.js"
+  },
+  {
+    "revision": "2d0ee0352f9573b384e2a4fa5a1e273c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "245d99862901137d7cd3ce42a1342b3b",
+    "url": "/img/logo.245d9986.jpg"
+  },
+  {
+    "revision": "cc4f270cd1eaddad9977956a9048a6c2",
+    "url": "/img/iconfont.cc4f270c.svg"
+  },
+  {
+    "revision": "d4b2d3d39858ca803b244ac8f57bf625",
+    "url": "/fonts/iconfont.d4b2d3d3.ttf"
+  },
+  {
+    "revision": "a0af713bddb13d7e9d3b4f76ad5446b6",
+    "url": "/fonts/iconfont.a0af713b.eot"
+  },
+  {
+    "revision": "41795facd9d80df2b9da",
+    "url": "/css/chunk-vendors.2763cfb3.css"
+  },
+  {
+    "revision": "bd29c8e90a04d0567e34",
+    "url": "/css/app.510bcdee.css"
+  }
+];
